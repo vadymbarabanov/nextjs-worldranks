6db8c883ea51dfa523679f8151ea164c6a57f6d2
Vadim Barabanov <introvert.png@gmail.com>
2021-07-10 13:39:34 +0300

    Fix typo

--- src/pages/country/[id].js
+++ src/pages/country/[id].js
@@ -111,7 +111,7 @@ export default Country;
 export const getStaticPaths = async () => {
   const res = await fetch("https://restcountries.eu/rest/v2/all");
   const countries = await res.json();
-  const paths = countries.map((counry) => ({
+  const paths = countries.map((country) => ({
     params: { id: country.alpha3Code },
   }));
 
